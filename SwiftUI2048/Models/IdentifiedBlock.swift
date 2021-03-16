a8a64797e67703b56d4aca46171196a95dd97313
mobileANDweb <mobiledeveloper112075@gmail.com>
2021-03-16 12:45:47 -0700

    Migrate to SwiftUI 2.0

---
+++ SwiftUI2048/Models/IdentifiedBlock.swift
@@ -0,0 +1,16 @@
+//
+//  IdentifiedBlock.swift
+//  SwiftUI2048
+//
+//  Created by David S on 6/5/19.
+//  Copyright © 2019 David S. All rights reserved.
+//
+
+import Foundation
+
+struct IdentifiedBlock: Block {
+    
+    let id: Int
+    var number: Int
+    
+}
